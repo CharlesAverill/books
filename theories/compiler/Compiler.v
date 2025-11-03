4afe10584955e2c809e4d6ff5c67c3898e9955ca
Charles Averill <charlesaverill20@gmail.com>
2025-11-03 02:53:46 -0600

    Start on CEVAL

--- theories/compiler/Compiler.v
+++ theories/compiler/Compiler.v
@@ -1,7 +1,9 @@
 From Books Require Import compiler.Routines compiler.Layout.
-From Books Require Import lang.Syntax vm.Syntax.
+From Books Require Import lang.Syntax vm.Syntax vm.FInterp.
 From Stdlib Require Import List.
 Import ListNotations.
+
+Definition LN {X : Type} := fun (x : list X) => N.of_nat (List.length x).
 
 Fixpoint AEVAL (pc : pc) (l : var_layout) (a : Aexpr) : list instr :=
   match a with
@@ -9,16 +11,16 @@ Fixpoint AEVAL (pc : pc) (l : var_layout) (a : Aexpr) : list instr :=
   | VarNat i => LOAD l i
   | lang.Syntax.Plus a1 a2 =>
       let l1 := AEVAL pc l a1 in
-      let l2 := AEVAL (pc + N.of_nat (length l1)) l a2
+      let l2 := AEVAL (pc + LN l1) l a2
       in l1 ++ l2 ++ [Plus]
   | lang.Syntax.Minus a1 a2 =>
       let l1 := AEVAL pc l a1 in
-      let l2 := AEVAL (pc + N.of_nat (length l1)) l a2
+      let l2 := AEVAL (pc + LN l1) l a2
       in l1 ++ l2 ++ [Minus]
   | lang.Syntax.Mult a1 a2 =>
       let l1 := AEVAL pc l a1 in
-      let l2 := AEVAL (pc + N.of_nat (length l1)) l a2
-      in l1 ++ l2 ++ MULT (pc + N.of_nat (length l1) + N.of_nat (length l2))
+      let l2 := AEVAL (pc + LN l1) l a2
+      in l1 ++ l2 ++ MULT (pc + LN l1 + LN l2)
   end.
 
 Fixpoint BEVAL (pc : pc) (l : var_layout) (b : Bexpr) : list instr :=
@@ -27,13 +29,46 @@ Fixpoint BEVAL (pc : pc) (l : var_layout) (b : Bexpr) : list instr :=
   | VarBool i => LOAD l i
   | lang.Syntax.Neg b =>
       let l1 := BEVAL pc l b in
-      l1 ++ NEG (pc + N.of_nat (length l1))
+      l1 ++ NEG (pc + LN l1)
   | lang.Syntax.Or b1 b2 =>
       let l1 := BEVAL pc l b1 in
       let l2 := BEVAL (pc + N.of_nat (length l1)) l b2
-      in l1 ++ l2 ++ OR (pc + N.of_nat (length l1) + N.of_nat (length l2))
+      in l1 ++ l2 ++ OR (pc + LN l1 + LN l2)
   | lang.Syntax.And b1 b2 =>
       let l1 := BEVAL pc l b1 in
       let l2 := BEVAL (pc + N.of_nat (length l1)) l b2
-      in l1 ++ l2 ++ AND (pc + N.of_nat (length l1) + N.of_nat (length l2))
+      in l1 ++ l2 ++ AND (pc + LN l1 + LN l2)
   end.
+
+Fixpoint CEVAL (pc : pc) (l : var_layout) (c : stmt) : list instr :=
+  match c with
+  | Skip => []
+  | Assign i e =>
+      let le := match e with AE e => AEVAL pc l e | BE e => BEVAL pc l e end in
+      le ++ SETN_fromstack (l i)
+  | Seq c1 c2 =>
+      let lc1 := CEVAL pc l c1 in
+      let lc2 := CEVAL (pc + LN lc1) l c2 in
+      lc1 ++ lc2
+  | IfThenElse b c1 c2 =>
+      let lb := BEVAL pc l (Neg b) in
+      let lc1 := CEVAL (pc + LN lb) l c1 in
+      let lc2 := CEVAL (pc + LN lb + LN lc1) l c2 in
+      lb ++ [Jnz (pc + LN lb + 1 + LN lc1 + 1)]
+         ++ lc1
+         ++ [Jmp (pc + LN lb + 1 + LN lc1 + 1 + LN lc2)]
+         ++ lc2
+  | While b cbody =>
+      let lb := BEVAL pc l b in
+      let lcbody := CEVAL (pc + N.of_nat (length lb) + 2) l cbody in
+      lb ++ [Jnz (pc + N.of_nat (length lb) + 2)]
+         ++ lcbody
+         ++ [Jmp pc]
+  end.
+
+Require Import String.
+Open Scope string.
+Definition plist := CEVAL 0 (fun _ => 0) (IfThenElse (ConstBool true) (Assign "hello" (AE (ConstNat 99))) Skip).
+Definition prog := program_of_plist plist.
+
+Compute fmultistep prog [] 0 (LN plist) 30.
